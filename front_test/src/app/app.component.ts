303003da29ca4bc8704002b6b3fc879b315d9475
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-09-06 08:44:56 -0500

    creating form

--- front_test/src/app/app.component.ts
+++ front_test/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { EmpleadosService } from './services/empleados/empleados.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front_test';
+export class AppComponent implements OnInit{
+
+  empleadoForm!: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    public empleadosService: EmpleadosService,
+
+  ){}
+  ngOnInit(): void {
+    this.empleadoForm = this.fb.group({
+      //numero_empleado : ['', Validators.required],
+      primer_apellido : ['', Validators.required],
+      segundo_apellido :['', Validators.required],
+      ciudad:['', Validators.required],
+      sexo:['', Validators.required],
+      centro:['', Validators.required],
+      puesto:['', Validators.required],
+      clave_interbancaria:['', Validators.required]
+    })
+  }
+
+  guardar(): void{
+
+  }
+
+
 }
